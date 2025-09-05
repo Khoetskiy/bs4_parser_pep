ade9b2841fde58004416bea168a8130cd9f2f2bc
Khoetskiy Sergey <khoetskiy@gmail.com>
2025-09-05 17:47:24 +0200

    Add new constants

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,8 @@ OUTPUT_PRETTY = 'pretty'
 OUTPUT_FILE = 'file'
 LOG_DIR = BASE_DIR / 'logs'
 LOG_FILE = LOG_DIR / 'parser.log'
+DOWNLOADS_DIR = BASE_DIR / 'downloads'
+RESULTS_DIR = BASE_DIR / 'results'
 
 MAIN_DOC_URL = 'https://docs.python.org/3/'
 PEP_URL = 'https://peps.python.org/'
